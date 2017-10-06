bbc2e2e919d59d4a9048cc1bc375c3c7ed1bb4b4
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-10-06 14:23:20 -0500

    started

---
+++ family_hash.rb
@@ -0,0 +1,24 @@
+my_family = {
+    :sister => {
+        :name => 'Claire',
+        :age => 35
+    },
+    :mother => {
+        :name => 'Jennifer',
+        :age => 60
+    },
+    :father => {
+        :name => 'Richard',
+        :age => 66
+    },
+    :brother1 => {
+        :name => 'Ben',
+        :age => 24
+    },
+    :brother2 => {
+        :name => 'Aaron',
+        :age => 25
+    }
+}
+
+puts "#{my_family[:name]}"
